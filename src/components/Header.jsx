65323277bba62b6f81c84837a4231ac2698247c9
Sahrul Romadi <sahrulromadi12@gmail.com>
2024-10-12 22:10:26 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../../public/assets/img/logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      
+    </header>
+  );
+};
+
+export default Header;
